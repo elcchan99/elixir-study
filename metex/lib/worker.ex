7f7814f1a3f5904a7114ef7fdc5cf8a5e3eb4566
edgar-chan@ <edgar.chan@cloverhealth.com>
2021-07-17 15:40:17 +0800

    Add The little Elixir & OTP 3.3 Metex

---
+++ metex/lib/worker.ex
@@ -0,0 +1,40 @@
+defmodule Metex.Worker do
+  def temperature_of(location) do
+    result = url_for(location) |> IO.inspect() |> HTTPoison.get() |> parse_response()
+
+    case result do
+      {:ok, temp} ->
+        "#{location}: #{temp}°C"
+
+      :error ->
+        "#{location} not found"
+    end
+  end
+
+  defp url_for(location) do
+    location = URI.encode(location)
+    "https://api.openweathermap.org/data/2.5/weather?q=#{location}&appid=#{apikey()}"
+  end
+
+  defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
+    body |> JSON.decode!() |> compute_temperature()
+  end
+
+  defp parse_response(res) do
+    IO.inspect(res, label: "response")
+    :error
+  end
+
+  defp compute_temperature(json) do
+    try do
+      temp = (json["main"]["temp"] - 273.15) |> Float.round(1)
+      {:ok, temp}
+    rescue
+      _ -> :error
+    end
+  end
+
+  defp apikey do
+    ""
+  end
+end
